0585b1cca83e6d4f41af52c2c9c03adc724c3b32
Jeonghyeon Kim <powergee101@gmail.com>
2021-08-09 03:36:05 +0900

    Initialize a dotnet project

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestcaseBruteforce
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
