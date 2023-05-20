a0a75575195cdd5cfeb43a91e0f5ebe80fefd855
Jeonghyeon Kim <powergee101@gmail.com>
2023-05-20 13:51:43 +0900

    Change symlink path to `/usr/local/bin`

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 sudo cp -r ./bin/Release/net6.0/publish /usr/local/tcbrute
 echo "Executable files are copied to /usr/local/tcbrute."
 
-if ! test -f "/usr/bin/tcbrute"; then
-    sudo ln -s /usr/local/tcbrute/TestcaseBruteforce /usr/bin/tcbrute
-    echo "Created a symbolic link. (/usr/local/tcbrute/TestcaseBruteforce -> /usr/bin/tcbrute)"
+if ! test -f "/usr/local/bin/tcbrute"; then
+    sudo ln -s /usr/local/tcbrute/TestcaseBruteforce /usr/local/bin/tcbrute
+    echo "Created a symbolic link. (/usr/local/tcbrute/TestcaseBruteforce -> /usr/local/bin/tcbrute)"
 fi
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,6 @@ if test -d "/usr/local/tcbrute"; then
     sudo rm -r /usr/local/tcbrute
 fi
 
-if test -f "/usr/bin/tcbrute"; then
-    sudo rm /usr/bin/tcbrute
+if test -f "/usr/local/bin/tcbrute"; then
+    sudo rm /usr/local/bin/tcbrute
 fi
